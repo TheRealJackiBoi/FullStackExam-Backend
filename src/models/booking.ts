5be0aef87bcbab4e30fc9bc62f2d62b5ede74a9f
jack <jslam@oulund.dk>
2024-03-18 21:50:28 +0100

    populate user create booking

--- src/models/booking.ts
+++ src/models/booking.ts
@@ -90,7 +90,11 @@ bookingSchema.pre("save", async function (next) {
     await this.populate({
       path: "company.owner",
       select: "-__v",
-    });
+    })
+    await this.populate({
+      path: "user",
+      select: "-__v",
+    })
     next();
   } catch (error: Error | any) {
     next(error);
